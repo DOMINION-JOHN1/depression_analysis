9589aa89e3e6b5a18cdef9ce5109c190fa69e610
John Dominion <jdominion123@gmail.com>
2023-09-25 13:05:19 +0100

    fastapi

---
+++ fastapi.py
@@ -0,0 +1,20 @@
+import pickle
+import sklearn
+from fastapi import FastAPI
+
+# Load the model from the pickle file
+with open('depression_analyzer.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Create a FastAPI instance
+app = FastAPI()
+
+# Define a function for making predictions
+@app.post("/predict")
+async def predict_depression(text: str):
+    try:
+        # Make predictions using the loaded model
+        prediction = model.predict([text])[0]
+        return {"prediction": prediction}
+    except Exception as e:
+        return {"error": str(e)}
