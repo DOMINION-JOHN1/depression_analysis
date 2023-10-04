1b2ff7ed73d299a6920fb365e517748b68e37198
John Dominion <jdominion123@gmail.com>
2023-10-04 09:20:18 +0100

    updates

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import pickle
-import sklearn
-from fastapi import FastAPI, HTTPException # Assuming 'FastAPI' is from 'fastapi' module, not 'main'
+import asyncio  # Import the asyncio module
+from fastapi import FastAPI, HTTPException
+import concurrent.futures
+import logging
+
 # Load the model from the pickle file
-with open('depression_analyzer.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-# Create a FastAPI instance
+try:
+    with open('depression_analyzer.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+except Exception as e:
+    # Handle model loading error
+    raise RuntimeError("Failed to load the model.") from e
+
 app = FastAPI()
-# Define a function for making predictions
+
+# Configure logging to a more appropriate level for production
+logging.basicConfig(level=logging.INFO)
+
+# Use a ThreadPoolExecutor to run predictions concurrently
+executor = concurrent.futures.ThreadPoolExecutor()
+
 @app.post("/predict")
 async def predict_depression(text: dict):
     try:
         input_text = text.get("text")
         if input_text is None:
             raise HTTPException(status_code=400, detail="Missing 'text' field in request")
-        # Make predictions using the loaded model
-        prediction = model.predict([input_text])[0]
+
+        # Define a function to make predictions
+        def predict(input_text):
+            return model.predict([input_text])[0]
+
+        # Submit the prediction task to the ThreadPoolExecutor
+        prediction = await asyncio.to_thread(predict, input_text)
         return {"prediction": prediction}
     except Exception as e:
-        raise HTTPException(status_code=422, detail=str(e))
+        # Log the error details
+        logging.error(f"Prediction error: {str(e)}")
+
+        # Return a more specific HTTPException with a 400 status code for prediction errors
+        raise HTTPException(status_code=400, detail="Prediction error")
--- version.py
+++ version.py
@@ -1,21 +1,32 @@
 import requests
+
 # Define the URL of your FastAPI application
 url = 'http://localhost:2000/predict'  # Replace with your actual URL
 
-# Define the input text
-input_text =input('Your input text: ')
+# Define the input texts as a list
+input_texts = [
+    "I am tired ",
+    "i feel like sleeping",
+    "i love food",
 
-# Send a POST request with JSON data
-response = requests.post(url, json={"text": input_text})
+]
 
-# Check the response
-if response.status_code == 200:
-    data = response.json()
-    if 'prediction' in data:
-        prediction = data['prediction']
-        print(f'Prediction: {prediction}')
-    elif 'error' in data:
-        error = data['error']
-        print(f'Error: {error}')
-else:
-    print(f'Request failed with status code {response.status_code}')
+# Loop through the input texts and send POST requests
+for input_text in input_texts:
+    # Send a POST request with JSON data
+    response = requests.post(url, json={"text": input_text})
+
+    # Check the response
+    if response.status_code == 200:
+        data = response.json()
+        if 'prediction' in data:
+            prediction = data['prediction']
+            print(f'Prediction for "{input_text}": {prediction}')
+        elif 'error' in data:
+            error = data['error']
+            print(f'Error for "{input_text}": {error}')
+        else:
+            print(f'Unexpected response format for "{input_text}": {data}')
+    else:
+        print(f'Request for "{input_text}" failed with status code {response.status_code}')
+        print(f'Response content: {response.text}')
